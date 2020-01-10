baa22cabe6acdf842c9970452f34bb63475adbba
manassuri7 <manassuri7@outlook.com>
2020-01-10 22:51:54 +0530

    second

---
+++ build/precache-manifest.d0c1196ca4ba3d665cf0fd098bc70331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c072a4ba79b3b2de9924748bbbecc2df",
+    "url": "/react-flexi-component/index.html"
+  },
+  {
+    "revision": "1074b2db02d19e7663fd",
+    "url": "/react-flexi-component/static/js/2.e2485674.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-flexi-component/static/js/2.e2485674.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bdb7fac206d909637ab",
+    "url": "/react-flexi-component/static/js/main.8a87c133.chunk.js"
+  },
+  {
+    "revision": "942d8d1518f1e8a0f453",
+    "url": "/react-flexi-component/static/js/runtime-main.e7cf1f85.js"
+  }
+]);
